05281a9ab1b466b64b7fcd61543965152dc7ba71
heyuansong <18742511352@163.com>
2020-05-21 20:49:33 +0800

    设置完成了！

--- src/main.c
+++ src/main.c
@@ -10,9 +10,7 @@ int main(int c,char**v){
   Mesh * meshes2[1] = {
      interface_Box(interface_shader("shader/tri.vs","shader/tri.fs"),
 					   -0.75,-0.75,0.5,0.5)
-  }; 
-  int a = 1;
-  int b = 2;
+  };
   window->connect_Mesh(window, meshes, 2);
   window->connect_Mesh(window, meshes2, 1);
   window->render(window);
